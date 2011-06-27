cd60f89a28f03e9b6aeca727d4508a2000844cbf
Flavio Curella <flavio.curella@gmail.com>
2011-06-27 16:51:15 -0500

    forgot to delete a refactored class
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- python_zipcodes/countries/us/importer.py
+++ python_zipcodes/countries/us/importer.py
@@ -5,42 +5,6 @@ import httplib2
 class ImproperlyConfiguredError(Exception):
     pass
 
-class GenericImporter(object):
-        
-    url = 'http://www.census.gov/tiger/tms/gazetteer/zips.txt'
-    txt = 'zipcodes.txt'
-    pickled = 'zipcodes.pickled'
-
-    def __init__(self, *args, **kwargs):
-        if self.url is None:
-            raise ImproperlyConfiguredError
-
-    def download(self):
-        client = httplib2.Http()
-        resp, content = client.request(self.url)
-        f = open(self.txt, 'wb')
-        f.write(content).close()
-        return content
-    
-    def parse(self, content):
-        raise NotImplementedError
-
-    def build_dict():
-        try:
-            content = open(self.txt, 'rb')
-        except IOError:
-            content = self.download()
-
-        return self.parse(content)
-
-    def zipcodes():
-        try:
-            records = pickle.load(open(self.pickled, 'rb'))
-        except IOError:
-            records = self.build_dict()
-            pickle.dump(records, open(self.pickled, 'wb'))
-        return records
-
 class Importer(GenericImporter):
         
     url = 'http://www.census.gov/tiger/tms/gazetteer/zips.txt'
@@ -51,6 +15,6 @@ class Importer(GenericImporter):
         for r in rows:
             zipcode = r[1]
             city = r[3].title()
-            state = r[2].title()
+            state = r[2].upper()
             records.setdefault(zipcode, [city, state])
         return records
