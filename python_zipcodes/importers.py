a7057c5ecf52eea5fce09e64221fd61900f9d745
Flavio Curella <flavio.curella@gmail.com>
2011-06-28 13:18:35 -0500

    refactor
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- python_zipcodes/importers.py
+++ python_zipcodes/importers.py
@@ -33,8 +33,8 @@ class GenericImporter(object):
         return StringIO(content)
     
     def update(self):
-        self.download()
-        self.zipcodes()
+        self.cached_data = self.parse(self.download())
+        self._write_to_db()
 
     def parse(self, content):
         raise NotImplementedError
@@ -49,28 +49,34 @@ class GenericImporter(object):
 
     def zipcodes(self):
         if not self.cached_data:
-            self.cached_data = self._read_from_db()
+            try:
+                self.cached_data = self._read_from_db()
+            except sqlite3.OperationalError:
+                self.conn.close()
+                self.cached_data = self.build_dict()
+                self._write_to_db()
         return self.cached_data
     
     def _read_from_db(self):
         c = self.conn.cursor()
         zipcodes = {}
-        try:
-            c.execute('select * from zipcodes')
-            for r in c:
-                zipcodes[r['zipcode']] = {'city':r['city'], 'state':r['state']}
-        except sqlite3.OperationalError:
-            zipcodes = self.build_dict()
-            c.execute("""drop table if exists zipcodes""")
-            c.execute("""create table zipcodes
-(zipcode text, city text, state text)""")
-            self.conn.commit()
-            for k, r in self.cache_data.items():
-                c.execute('insert into zipcodes values (?,?,?)', [k, r['city'], r['state']])
-            self.conn.commit()
+        c.execute('select * from zipcodes')
+        for r in c:
+            zipcodes[r['zipcode']] = {'city':r['city'], 'state':r['state']}
         c.close()
         return zipcodes
-        
+    
+    def _write_to_db(self):
+        c = self.conn.cursor()
+        c.execute("""drop table if exists zipcodes""")
+        c.execute("""create table zipcodes
+(zipcode text, city text, state text)""")
+        self.conn.commit()
+        for k, r in self.cached_data.items():
+            c.execute('insert into zipcodes values (?,?,?)', [k, r['city'], r['state']])
+        self.conn.commit()
+        c.close()
+
     def __getitem__(self, key):
         if not self.cached_data:
             self.cached_data = self.zipcodes()
