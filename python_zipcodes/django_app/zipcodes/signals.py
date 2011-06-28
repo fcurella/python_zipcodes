ef3d1d06f25ef3231bebc7cb85cc1420b6a2c3fc
Flavio Curella <flavio.curella@gmail.com>
2011-06-28 16:35:26 -0500

    signals
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- python_zipcodes/django_app/zipcodes/signals.py
+++ python_zipcodes/django_app/zipcodes/signals.py
@@ -3,9 +3,10 @@ from python_zipcodes.importers import ZipCodeManager
 
 def save_zipcodes(sender, app, created_models, **kwargs):
 
-    zc_manager = ZipCodeManager()
-    for c in country_list:
-        zc_manager.add(c)
-        for zc, data in zc_manager.zipcodes(c).items():
-            sender.ZipCode.objects.create(zipcode=zc, city=data['city'], state=data['state'], country=c)
+    if sender.ZipCode.objects.count() == 0:
+        zc_manager = ZipCodeManager()
+        for c in country_list:
+            zc_manager.add(c)
+            for zc, data in zc_manager.zipcodes(c).items():
+                sender.ZipCode.objects.create(zipcode=zc, city=data['city'], state=data['state'], country=c)
 
