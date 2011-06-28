492f8e5c18797f5c44e69c52fe97ce5721989888
Flavio Curella <flavio.curella@gmail.com>
2011-06-28 10:30:50 -0500

    cleaned up us importer
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- python_zipcodes/countries/us/importer.py
+++ python_zipcodes/countries/us/importer.py
@@ -3,9 +3,6 @@ import pickle
 import httplib2
 from python_zipcodes.importers import GenericImporter
 
-class ImproperlyConfiguredError(Exception):
-    pass
-
 class Importer(GenericImporter):
     url = 'http://www.census.gov/tiger/tms/gazetteer/zips.txt'
     country = 'us'
