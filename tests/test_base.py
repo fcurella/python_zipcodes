1588001cd18b0b060e5677a06c943d79c3e54b39
Flavio Curella <flavio.curella@gmail.com>
2011-06-29 09:31:11 -0500

    updated tests for the new api
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,7 +3,7 @@ from nose.tools import assert_equal, assert_not_equal, assert_raises, assert_tru
 
 def test_basics():
     manager = ZipCodeManager()
-    zipcodes = manager.get('US')
+    zipcodes = manager.zipcodes('US')
     zipcode = '66044'
     assert_true(zipcodes['us'].has_key(zipcode))
     assert_equal(zipcodes['us'][zipcode], {'city':'Lawrence', 'state':'KS'})
