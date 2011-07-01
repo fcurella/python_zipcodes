f4e006dfb456e647cd2bd97d5938896c5808ba02
Flavio Curella <flavio.curella@gmail.com>
2011-07-01 15:36:20 -0500

    don't add all the zipcodes if we are running tests
    Signed-off-by: Flavio Curella <flavio.curella@gmail.com>

--- python_zipcodes/django_app/zipcodes/signals.py
+++ python_zipcodes/django_app/zipcodes/signals.py
@@ -1,5 +1,6 @@
 from python_zipcodes.countries import country_list
 from python_zipcodes.importers import ZipCodeManager
+import sys
 
 def save_zipcodes(sender, app, created_models, **kwargs):
 
@@ -7,6 +8,10 @@ def save_zipcodes(sender, app, created_models, **kwargs):
     for c in country_list:
         zc_manager.add(c)
         if sender.ZipCode.objects.filter(country=c).count() == 0:
-            for zc, data in zc_manager[c].zipcodes().items():
+            if sys.argv[1] == 'test':
+                zipcodes = [('10000', {'city': 'Test City', 'state': 'Test State'})]
+            else:
+                zipcodes = zc_manager[c].zipcodes().items()
+            for zc, data in zipcodes:
                 sender.ZipCode.objects.create(zipcode=zc, city=data['city'], state=data['state'], country=c)
 
